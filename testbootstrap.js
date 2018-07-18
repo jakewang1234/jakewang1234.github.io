4ac571c17b069b46da9ca96a65906dec16cc1e7e
jakewang1234 <41014394+jakewang1234@users.noreply.github.com>
2018-07-18 11:12:49 -0400

    test
    test

---
+++ testbootstrap.js
@@ -0,0 +1,13 @@
+var value1 = 10;
+var value2 = 15;
+
+function multiply (v1, v2){
+  var multiplied = value1 * value2;
+  alert(multiplied);
+  return (multiplied);
+}
+// alert multiply(value1, value2);
+
+var emailField = document.getElementbyID('exampleInputEmail1');
+
+emailField.value = "yourname@gmail.com";
